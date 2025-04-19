e16a7632586d71deecc65a565ceec73b314525d0
LaurenceLin <laurencelin0614@icloud.com>
2025-04-19 11:02:22 +0800

    Initial Commit

---
+++ HW43_TableView_Practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW43_TableView_Practice
+//
+//  Created by 林上閔 on 4/19/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ HW43_TableView_PracticeTests/HW43_TableView_PracticeTests.swift
@@ -0,0 +1,17 @@
+//
+//  HW43_TableView_PracticeTests.swift
+//  HW43_TableView_PracticeTests
+//
+//  Created by 林上閔 on 4/19/25.
+//
+
+import Testing
+@testable import HW43_TableView_Practice
+
+struct HW43_TableView_PracticeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
